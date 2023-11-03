6ec4706df54e7f8f76649330aa832a0d2f5efe7f
kartikkotkar <kartikkotkar973@gmail.com>
2023-11-03 00:56:41 -0700

    added code

--- client/src/views/BuyPage/BuyPage.js
+++ client/src/views/BuyPage/BuyPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./BuyPage.css";
-import { useParams } from "react-router-dom";
+import { json, useParams } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import axios from "axios";
 import { useState, useEffect } from "react";
@@ -10,14 +10,46 @@ function BuyPage() {
 
     const { id } = useParams();
     const [product, setProduct] = useState();
-    const loadProducts = async () => {
+    const [quantity, setQuantity] = useState(1);
+    const [shippingAddress, setShippingAddress] = useState('');
 
+    const loadProducts = async () => {
         if (!id) {
             return;
         }
         const response = await axios.get(`/product/${id}`);
         setProduct(response?.data?.date[0]);
     };
+
+    const increaseQuantity = () => {
+        setQuantity(quantity + 1);
+    }
+
+    const decreaseQuantity = () => {
+        if (quantity === 1) {
+            return;
+        }
+        setQuantity(quantity - 1);
+    }
+
+    const placeOrder = async () => { 
+        const currentUser = JSON.parse(localStorage.getItem('user') || '{}');
+        const orderDetails = {
+            user: currentUser._id,
+            product: id,
+            quantity: quantity,
+            shippingAddress: shippingAddress
+        }
+
+        const response = await axios.post('/order', orderDetails);
+
+        alert(response?.data?.message);
+        if(response?.data?.success) {
+            Window.location.href = '/orders';
+        }
+    }
+
+
     useEffect(() => {
         loadProducts();
     }, []);
@@ -36,8 +68,27 @@ function BuyPage() {
                         <h1>₹ {product?.price}</h1>
                         <h1>{product?.name}</h1>
                         <p>{product?.description}</p>
+
+                        <div>
+
+                            <span className="btn-decrease-quantity" onClick={decreaseQuantity}>➖</span>
+                            <span className="product-quantity-text">{quantity}</span>
+                            <span className=" btn-increase-quantity" onClick={increaseQuantity}>➕</span>
+
+                        </div>
+                        <input type="text"
+                            placeholder="Enter Shipping address"
+                            className="input-shipping-address"
+                            value={shippingAddress}
+                            onChange={(e) => setShippingAddress(e.target.value)}
+                        />
                     </div>
+
                 </div>
+                <button type="button" className="btn btn-place-order"
+                    onClick={placeOrder}>
+                    place Order
+                </button>
             </div>
         </div>
     )
